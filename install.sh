f7dd52bc6fb578e5375c19c3ad1f972e6b8cdd92
Lin314159 <danielamdurer@gmail.com>
2015-07-02 18:20:37 +0100

    Debugged install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,4 @@
 #! /bin/bash
-foo=0
-start
-while [foo -eq 0]
-do echo "Choose version"
-echo " 1 = Openbox-based desktop "
-echo " 2 = light app suite "
-echo " 3 = both "
-read choice
-case $choice in
-1) install1
-2) install2
-3) install2 && install1
-q) exit
-*)	echo "Choose 1, 2, 3. or q to exit"
-esac
-done
 start () {
 echo "This is the Iced Balloon Installer. It requires a working Arch system."
 echo ""
@@ -41,3 +25,19 @@ foo=0
 yaourt -Syu lxappearance abiword deadbeef midori evince gimp gnumeric grsync liferea lyx osmo pidgin vlc transmission-gtk sylpheed xfburn xfce4-taskmanager --insecure
 foo=1
 }
+foo=0
+start
+while [foo -eq 0]
+do echo "Choose version"
+echo " 1 = Openbox-based desktop "
+echo " 2 = light app suite "
+echo " 3 = both "
+read choice
+case $choice in
+1) install1;;
+2) install2;;
+3) install2 && install1;;
+q) exit;;
+*)	echo "Choose 1, 2, 3. or q to exit";;
+esac
+done
