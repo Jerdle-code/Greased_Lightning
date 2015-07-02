16333dda260548b5d714ae0310f5d219766c668a
Lin314159 <danielamdurer@gmail.com>
2015-07-02 21:04:23 +0100

    Change to Whiptail, rather than the kludge of a new variable. This is version 3.0!

--- installtext.sh
+++ installtext.sh
@@ -14,17 +14,30 @@ echo "Enabling AUR"
 pacman -Syyu yaourt
 } 
 install1() {
+foo=0
 yaourt -Syu pcmanfm lxappearance cairo-compmgr wbar xfce4-panel mint-backgrounds-qiana conky openbox obconf lxappearance-obconf gmrun obkey sddm --insecure
 systemctl enable sddm
 echo "Log into Openbox"
+foo=1
 }
 install2() {
+foo=0
 yaourt -Syu lxappearance abiword deadbeef midori evince gimp gnumeric grsync liferea lyx osmo pidgin vlc transmission-gtk sylpheed xfburn xfce4-taskmanager --insecure
+foo=1
 }
+foo=0
 start
-choices=$(whiptail --checklist "Choose programs:" 10 40 2 \
-        1 "Desktop" on \
-        2 "App suite" on \
- 3>&1 1>&2 2>&3)
-echo $choices | grep 1 > /dev/null && install1
-echo $choices | grep 2 > /dev/null && install1
+while [ $foo -eq 0 ]
+do echo "Choose version"
+echo " 1 = Openbox-based desktop "
+echo " 2 = light app suite "
+echo " 3 = both "
+read choice
+case $choice in
+1) install1;;
+2) install2;;
+3) install2 && install1;;
+q) exit;;
+*)	echo "Choose 1, 2, 3. or q to exit";;
+esac
+done
